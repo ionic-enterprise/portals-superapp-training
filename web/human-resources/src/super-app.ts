154a048968a6e1111255d8a30191d1d1fcf5e497
Eric Horodyski <horodyski@ionic.io>
2023-07-06 14:19:01 -0400

    step(two): implement a Capacitor plugin for communication

--- web/human-resources/src/super-app.ts
+++ web/human-resources/src/super-app.ts
@@ -1,3 +1,4 @@
+import { registerPlugin } from "@capacitor/core";
 import { getInitialContext } from "@ionic/portals";
 
 export type Context = {
@@ -9,4 +10,9 @@ export type Context = {
   resourceId: number;
 };
 
+interface DismissPlugin {
+  dismiss(): Promise<void>;
+}
+
+export const dismissPlugin = registerPlugin<DismissPlugin>("Dismiss", {});
 export const initialContext = getInitialContext<Context>()!.value!;
